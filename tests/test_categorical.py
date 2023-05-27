87b049c9c41c3d43811d51965cd9493aa5446145
GarrettPetersen <garrett.m.petersen@gmail.com>
2023-05-27 08:52:55 -0700

    Update spacing

--- tests/test_categorical.py
+++ tests/test_categorical.py
@@ -18,6 +18,9 @@ def test_categorical_graph():
     graph.primary_y_axis_label = "Primary Y Axis"
 
     graph.add_series([10, 20, 30, 40, 50], legend_label="Series 1")
+    graph.add_series([15, 25, 5, 44, 56], legend_label="Series 2")
+    graph.add_series([5, 35, 10, 33, 40], legend_label="Series 3", series_type="line")
+    graph.add_series([35, 56, 25, 5, 44], legend_label="Series 4", series_type="dot")
 
     # Get the SVG string in base64 format
     svg_base64 = graph.to_base64_src()
@@ -28,4 +31,10 @@ def test_categorical_graph():
     # Check that the result is long enough to be a non-trivial SVG
     assert len(svg_base64) > 100
 
-    print(f"<img src='{svg_base64}' />")
+    print(f"\n<img src='{svg_base64}' />")
+
+    graph.stacked = True
+
+    stacked_base64 = graph.to_base64_src()
+
+    print(f"\n<img src='{stacked_base64}' />")
