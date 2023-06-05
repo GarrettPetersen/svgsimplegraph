aea200ee08441b19b77b3079d2fb3ac99d35a9e3
GarrettPetersen <garrett.m.petersen@gmail.com>
2023-06-05 11:07:40 -0700

    update test with different scale

--- tests/test_categorical.py
+++ tests/test_categorical.py
@@ -22,7 +22,10 @@ def test_categorical_graph():
     graph.add_series([15, 25, -5, 44, 56], legend_label="Series 2")
     graph.add_series([5, 35, 10, 33, 40], legend_label="Series 3", series_type="line")
     graph.add_series(
-        [35, 56, 25, 5, 44], legend_label="Series 4", series_type="dot", secondary=True
+        [0.35, 0.56, 0.25, 0.05, 0.44],
+        legend_label="Series 4",
+        series_type="dot",
+        secondary=True,
     )
 
     # Get the SVG string in base64 format
