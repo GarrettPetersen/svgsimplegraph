a15cb2d2d5ca48f2ddecd484c2c640ddbd90abe2
GarrettPetersen <garrett.m.petersen@gmail.com>
2023-06-09 22:00:12 -0700

    add watermark to test

--- tests/test_categorical.py
+++ tests/test_categorical.py
@@ -9,6 +9,7 @@ def test_categorical_graph():
         height=400,
         bar_width=30,
         title="Categorical Graph",
+        watermark="<rect x='250' y='150' width='100' height='100' fill='rgba(255, 0, 0, 0.5)' />",
     )
 
     graph.x_labels = ["A", "B", "C", "D", "E"]
