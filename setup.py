2a14030ada40dc40cd35d5afc49ca501d64dd96c
GarrettPetersen <59745076+GarrettPetersen@users.noreply.github.com>
2024-02-13 16:15:24 -0800

    Correct vertical line positions (#7)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 DESCRIPTION = "Simple SVG graphing package"
 
 setup(
